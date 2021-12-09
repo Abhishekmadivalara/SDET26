0b9ca00087a413ef5361938921192ef968f41742
HP <HP@192.168.2.46>
2021-12-09 15:38:32 +0530

    TestCase

--- src/test/java/SDETpractice/Test05.java
+++ src/test/java/SDETpractice/Test05.java
@@ -1,5 +1,14 @@
 package SDETpractice;
 
+import org.testng.annotations.Test;
+
 public class Test05 {
 
+	  @Test
+	  
+	  public void test()
+	  {
+		  
+		  System.out.println("hello data driven testing");
+}
 }
